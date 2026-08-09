119748e88fbb4e61e2826a907ebdbb94c415183a
Zoe Shortman <zshortma@asu.edu>
2026-08-09 16:54:33 -0500

    Use SQLC to generate go code.

---
+++ Database-Migration/Database-Migration/query/account.sql
@@ -0,0 +1,28 @@
+-- name: CreateAccount :one
+INSERT INTO ksbnk_accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2, $3
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM ksbnk_accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM ksbnk_accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE ksbnk_accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM ksbnk_accounts
+WHERE id = $1;
---
+++ Database-Migration/go.mod
@@ -0,0 +1,3 @@
+module github.com/KansasFirstBank
+
+go 1.26.5
