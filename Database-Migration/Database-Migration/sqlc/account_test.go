9b1d38bebdd2802a86740708a95923e259e876be
Zoe Shortman <zshortma@asu.edu>
2026-08-10 20:46:45 -0500

    GoLang Unit test for TablePlus DB inserts.

---
+++ Database-Migration/Database-Migration/sqlc/account_test.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateAccount(t *testing.T) {
+	args := CreateAccountParams{
+		Owner:    "Zoe Doe",
+		Balance:  1000,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), args)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
+
+	require.NotZero(t, account.ID)
+	require.NotZero(t, account.CreatedAt)
+}
